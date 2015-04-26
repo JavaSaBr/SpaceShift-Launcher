09f86c54f53129d706e04a089c3dfc95b5702be4
Ronn <aliaksandr.brui@instinctools.ru>
2015-04-26 09:36:47 +0300

    наработки

--- Game Launcher/src/com/ss/launcher/ui/LauncherUIWindow.java
+++ Game Launcher/src/com/ss/launcher/ui/LauncherUIWindow.java
@@ -1,11 +1,23 @@
 package com.ss.launcher.ui;
 
-import static com.ss.client.fx.ui.css.CSSIds.BLACK_BLUE_GRADIENT_BACKGROUND;
-import static com.ss.client.fx.ui.css.CSSIds.GAME_DRAGGABLE_PANEL_BUTTON_CLOSE;
+import static javafx.geometry.Pos.CENTER;
+import static javafx.geometry.Pos.CENTER_RIGHT;
+import static javafx.scene.paint.Color.TRANSPARENT;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import rlib.ui.hanlder.WindowDragHandler;
 import rlib.ui.page.UIPage;
+import rlib.ui.util.FXUtils;
+import rlib.ui.window.UIWindow;
 import rlib.ui.window.impl.UndecoratedUIWindow;
 import rlib.util.array.Array;
 
@@ -16,25 +28,94 @@ import rlib.util.array.Array;
  */
 public class LauncherUIWindow extends UndecoratedUIWindow {
 
+	public static final String CSS_FILE_BASE = "/ui/fx/css/base.css";
+	public static final String CSS_FILE_EXTERNAL = "/ui/fx/css/external.css";
+	public static final String CSS_FILE_CUSTOM_IDS = "/ui/fx/css/custom_ids.css";
+	public static final String CSS_FILE_CUSTOM_CLASSES = "/ui/fx/css/custom_classes.css";
+
+	private static final Insets PROP_CLOSE_BUTTON_OFFSET = new Insets(4, 40, 0, 0);
+	private static final Insets PROP_TITLE_OFFSET = new Insets(10, 0, 0, 0);
+
+	public static final int WINDOW_WIDTH = 1000;
+	public static final int WINDOW_HEIGHT = 500;
+
 	public LauncherUIWindow(Stage stage, Array<Class<? extends UIPage>> availablePages) {
 		super(stage, availablePages);
 	}
 
 	@Override
+	protected Stage configureStage(Stage stage) {
+		stage.initStyle(StageStyle.TRANSPARENT);
+		stage.setResizable(false);
+		stage.setMaxHeight(WINDOW_HEIGHT);
+		stage.setMinHeight(WINDOW_HEIGHT);
+		stage.setMaxWidth(WINDOW_WIDTH);
+		stage.setMinWidth(WINDOW_WIDTH);
+		return stage;
+	}
+
+	/**
+	 * Создание и определение сцены {@link UIWindow}.
+	 */
+	@Override
+	protected Scene createdScene() {
+
+		final Scene scene = new Scene(getRootNode(), WINDOW_WIDTH, WINDOW_HEIGHT, Color.TRANSPARENT);
+		scene.setFill(TRANSPARENT);
+
+		final ObservableList<String> stylesheets = scene.getStylesheets();
+		stylesheets.add(CSS_FILE_BASE);
+		stylesheets.add(CSS_FILE_EXTERNAL);
+		stylesheets.add(CSS_FILE_CUSTOM_IDS);
+		stylesheets.add(CSS_FILE_CUSTOM_CLASSES);
+
+		return scene;
+	}
+
+	@Override
 	protected Pane createRoot() {
+
 		final Pane root = super.createRoot();
-		root.setId(BLACK_BLUE_GRADIENT_BACKGROUND);
+		root.setId("BlackBlueGradientBackground");
+
 		return root;
 	}
 
 	@Override
 	protected Button createCloseButton() {
 
-		final Button button = new Button();
-		button.setId(GAME_DRAGGABLE_PANEL_BUTTON_CLOSE);
+		final Button button = new Button("X");
+		button.setId("GameDraggablePanelButtonClose");
 		button.setOnAction(event -> close());
 
 		return button;
+	}
+
+	@Override
+	protected void applyMarginToCloseButton(Button closeButton) {
+		StackPane.setMargin(closeButton, PROP_CLOSE_BUTTON_OFFSET);
+	}
+
+	@Override
+	protected Pane createHeader() {
+
+		final StackPane header = new StackPane();
+		header.setAlignment(CENTER_RIGHT);
+
+		Label titleLabel = new Label("SpaceShift Launcher");
+		titleLabel.setTextAlignment(TextAlignment.CENTER);
+		titleLabel.setAlignment(CENTER);
+
+		FXUtils.bindFixedWidth(titleLabel, header.widthProperty());
+
+		FXUtils.addToPane(titleLabel, header);
+		FXUtils.addClassTo(titleLabel, "arial-label-20-bold-with-shadow");
+
+		StackPane.setMargin(titleLabel, PROP_TITLE_OFFSET);
+
+		WindowDragHandler.install(header);
+
+		return header;
 	}
 
 	@Override
--- Game Launcher/src/com/ss/launcher/ui/page/MainUIPage.java
+++ Game Launcher/src/com/ss/launcher/ui/page/MainUIPage.java
@@ -1,10 +1,32 @@
 package com.ss.launcher.ui.page;
 
 import static javafx.geometry.Pos.TOP_CENTER;
+
+import java.awt.Point;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
 import rlib.ui.page.impl.AbstractUIPage;
+import rlib.ui.util.FXUtils;
 import rlib.ui.window.UIWindow;
+import rlib.util.Util;
+
+import com.ss.launcher.Launcher;
 
 /**
  * Реализация основной страницы лаунчера.
@@ -13,7 +35,17 @@ import rlib.ui.window.UIWindow;
  */
 public class MainUIPage extends AbstractUIPage {
 
+	public static final Insets PROP_LINE_OFFSET = new Insets(10, 0, 10, 0);
+
 	private VBox root;
+
+	private ImageView logoImageView;
+
+	private Button updateClient;
+
+	private Button playButton;
+
+	private ProgressBar progressBar;
 
 	@Override
 	public void postPageShow(UIWindow window) {
@@ -29,6 +61,132 @@ public class MainUIPage extends AbstractUIPage {
 		root = new VBox();
 		root.setAlignment(TOP_CENTER);
 
+		createSplitLine(root);
+		createLogo();
+		createProgressBar();
+		createButtons();
+
 		return root;
+	}
+
+	protected void createProgressBar() {
+
+		progressBar = new ProgressBar(0);
+		progressBar.setId("HangarProgressStrength");
+
+		VBox.setMargin(progressBar, new Insets(10, 0, 0, 0));
+
+		FXUtils.setFixedSize(progressBar, new Point(800, 10));
+		FXUtils.addToPane(progressBar, root);
+	}
+
+	protected void createButtons() {
+
+		HBox buttonContainer = new HBox();
+		buttonContainer.setAlignment(Pos.CENTER);
+
+		updateClient = new Button();
+		updateClient.setText("Проверить обновление");
+		updateClient.setOnAction(event -> processUpdate());
+
+		playButton = new Button();
+		playButton.setText("Играть");
+		playButton.setOnAction(event -> processPlay());
+
+		FXUtils.addClassTo(updateClient, "arial-label-17");
+		FXUtils.addClassTo(playButton, "arial-label-17");
+		FXUtils.setFixedSize(updateClient, new Point(300, 26));
+		FXUtils.setFixedSize(playButton, new Point(300, 26));
+
+		FXUtils.addToPane(updateClient, buttonContainer);
+		FXUtils.addToPane(playButton, buttonContainer);
+		FXUtils.addToPane(buttonContainer, root);
+
+		VBox.setMargin(buttonContainer, new Insets(10, 0, 0, 0));
+		HBox.setMargin(updateClient, new Insets(0, 5, 0, 0));
+		HBox.setMargin(playButton, new Insets(0, 0, 0, 5));
+	}
+
+	private void processUpdate() {
+
+	}
+
+	protected void createLogo() {
+
+		logoImageView = new ImageView();
+		logoImageView.setImage(new Image("/com/ss/launcher/resources/logo.png"));
+
+		FXUtils.addToPane(logoImageView, root);
+	}
+
+	private void processPlay() {
+		processRun();
+	}
+
+	protected void processRun() {
+
+		final Path rootFolder = Util.getRootFolderFromClass(Launcher.class);
+		final Path targetFile = Paths.get(rootFolder.toString(), "game", "spaceshift.jar");
+
+		Path javaFolder = Paths.get(System.getProperty("java.home"), "bin");
+		Path targetJava = javaFolder.resolve("java");
+
+		if(!Files.exists(targetJava)) {
+			targetJava = javaFolder.resolve("java.exe");
+		}
+
+		if(!Files.exists(targetJava)) {
+			javaFolder = Paths.get(System.getProperty("java.home"), "lib");
+			// targetJava = javaFolder.resolve("jexec");
+		}
+
+		if(!Files.exists(targetJava)) {
+			// targetJava = javaFolder.resolve("jexec.exe");
+		}
+
+		// System.out.println("properties: ");
+
+		for(Entry<Object, Object> entry : System.getProperties().entrySet()) {
+			// System.out.println(entry.getKey() + " = " + entry.getValue());
+		}
+
+		String java = Files.exists(targetJava) ? targetJava.toString() : "java";
+
+		List<String> commands = new ArrayList<>();
+		commands.add(java);
+		commands.add("-jar");
+		commands.add("-XX:CompileThreshold=200");
+		commands.add("-XX:+AggressiveOpts");
+		commands.add("-XX:CompileThreshold=200");
+		commands.add("-XX:+UseParallelGC");
+		commands.add("-XX:+UseTLAB");
+		commands.add("-Xmx512m");
+		commands.add(targetFile.toString());
+
+		System.out.println("commands " + commands);
+
+		ProcessBuilder builder = new ProcessBuilder(commands);
+		builder.inheritIO();
+
+		try {
+			final Process process = builder.start();
+			process.waitFor();
+		} catch(IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Создание разделительной линии.
+	 */
+	protected void createSplitLine(final VBox container) {
+
+		final Line splitLine = new Line();
+		splitLine.setId("BlueGradientHorizontalLine");
+		splitLine.setStartX(0);
+		splitLine.endXProperty().bind(container.widthProperty());
+
+		VBox.setMargin(splitLine, PROP_LINE_OFFSET);
+		FXUtils.addToPane(splitLine, container);
 	}
 }
